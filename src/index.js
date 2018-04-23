0dca5901efb7212b080b274a50d0218933c2784b
nano <ncysatnaf@gmail.com>
2018-04-23 12:26:24 +0800

    update template

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import Menhera from "menhera";
 
 const _ = new Menhera({
   _hooks: {
-    say({ _val }) {
-      console.log(_val);
+    say: {
+      _({ _val }) {
+        console.log(_val);
+      }
     }
-  },
-  say: "Hello World"
+  }
 });
+
+_.$use({ say: "Hello World" });
+_.$use([{ say: "foo" }, { say: "bar" }]);
